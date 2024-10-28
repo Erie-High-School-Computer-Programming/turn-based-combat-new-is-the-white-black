a70839c64a26631b344ad67a53aaccb269db9fa4
secretx09 <101412985+secretx09@users.noreply.github.com>
2024-10-28 15:08:32 +0000

    WOOHOOOOOO

--- main.py
+++ main.py
@@ -3,25 +3,25 @@ import pygame
 # and fight against each other. The game should have a simple combat system
 # where characters can deal damage to each other. The game should also have
 # a way to display the current health of each character.
-
+ 
 # The game should have turned based combat where each player takes turns
 # Players should have an attack, defense, and health stat
-
+ 
 # Combat should involve a level of randomness.
-
+ 
 # The game should have a way to display the current health of each character after each turn.
-
+ 
 # Combat should continue until one of the characters health reaches 0.
-
+ 
 # The game should have a way to display the winner of the game.
-
+ 
 # The game should have a way to restart the game.
-
+ 
 # The game should have a way to exit the game.
 import sys
 import random
 from player import Player
-
+ 
 class Game:
     def __init__(self):
         """Initializes the game,
@@ -32,73 +32,93 @@ class Game:
         then have the computer choose a character at random
         It should randomly select a player to go first"""
         print("Hello and welcome")
-        puppaydog = Player("Puppay", "Tornadotwisty", "PuppayDog Eyes", 100, move_list())
-        
+        puppaydog = Player("Puppay", "Tornadotwisty", "PuppayDog Eyes", 100, move_list =
+        {
+            "Puppay": {"damage": 150, "accuracy": 50},
+            "Tornadotwisty": {"damage": 350, "accuracy": 20},
+            "PuppayDog Eyes": {"damage": 50, "accuracy": 90}
+        })
+        kittaycat = Player("cat", "the-wip", "kungfu", 100, move_list =
+        {
+            "cat": {"damage": 130, "accuracy": 65 },
+            "The-wip": {"damage": 370, "accuracy": 7},
+            "kungfu": {"damage": 75, "accuracy": 80}
+        })
+ 
+
+        choice = input("Which player would you like to be? puppaydog (a) or kittaycat (b) >>")
+        if choice == "a":
+            self.player = puppaydog
+        elif choice == "b": 
+            self.player = kittaycat
+        else: 
+            print("Invalid response. Please pick a or b.")
+            choice = input("Which player would you like to be? puppaydog (a) or kittaycat (b) >>")
 
         self.player_turn = True # False will mean the computers turn
         #print(self.current_turn)
-
+ 
     def turn(self, current_turn):
-        """This method should show the current health of both players, 
+        """This method should show the current health of both players,
         and allow the player to select a move to use on the opponent
         If it is the computer player's turn, it should select a move at random"""
         if self.player_turn:
             move = input("Pick your attack")
             if move == "quit":
                 self.exit()
-            #else:
-                #player.attack(computer)
-
+            else:
+                player.attack(computer)
+ 
         else:
             #move = random.choice(self.computer.movelist) or movelist.kes() if dict
             #computer.attack(player)
             pass
-
+ 
         self.player_turn = not self.player_turn
-
+ 
     def check_winner(self):
         """This method should check if either player's health has reached 0
         If a player's health has reached 0, it should display the winner"""
         if player.health <= 0:
             check_winner()
-
+ 
     def restart(self):
         """This method should allow the player to restart the game"""
-        
-        
-
+        pass
+       
+ 
     def exit(self):
         """This method should allow the player to exit the game"""
         sys.exit()
-
-
+ 
+ 
     def run(self):
         self.player_turn = random.choice(True, False)
         player_choice = input("Choose your character!")
         #Create player object Here
         #Choose computer character
         self.playing = True
-        while self.playing
-        while True:
-            self.turn()
-            self.check_winner()
+        while self.playing:
+            while True:
+                self.turn()
+                self.check_winner()
        
-
-
-
-    
+ 
+ 
+ 
+   
 def main():
     game = Game()
     game.run()
-
-
-
-#This calls the main function 
+ 
+ 
+ 
+#This calls the main function
     game = Game()
     game.turn(1)
     game.check_winner()
     game.turn(2)
     game.check_winner()
-
+ 
 #This calls the main function
 main()
--- player.py
+++ player.py
@@ -1,5 +1,7 @@
+import random
+
 class Player:
-    def __init__(self, name, attack, defense, health_stat, attack_list):
+    def __init__(self, name, attack, defense, health_stat, attack_list, move_list):
         """This method initializes the character
         It should give the character a name, attack, defense, and health stat
         It should also give the character a movelist"""
@@ -9,15 +11,19 @@ class Player:
         self.health_stat = health_stat
         self.attack_list = attack_list
         self.hp = 1000
-
+        self.move_list = move_list
+ 
     def attack(self, move, target):
-        """This method should allow the character to attack another character using the 
+        """This method should allow the character to attack another character using the
         selected move. The move should deal damage to the target character"""
-        pass
 
+        if random.randint(0, 100) < self.move_list[move]["accuracy"]:
+            target.hp -= self.move_list[move]["damage"]
+            print(f"{target} was Hit")
+        else:
+            print("Attack missed lil bro")
+    
     def display_stats(self):
         """This method should display the current health of the character"""
         print(self.name)
         print(self.hp)
-
-
