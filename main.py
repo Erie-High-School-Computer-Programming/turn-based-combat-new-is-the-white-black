3a19e653cfc03e27f10db161c361998ff184b725
secretx09 <101412985+secretx09@users.noreply.github.com>
2024-10-30 15:34:17 +0000

    done??

--- main.py
+++ main.py
@@ -32,13 +32,13 @@ class Game:
         then have the computer choose a character at random
         It should randomly select a player to go first"""
         print("Hello and welcome")
-        self.puppaydog = Player("Puppay", "Tornadotwisty", "PuppayDog Eyes", 100, move_list =
+        self.puppaydog = Player("Puppay", "Tornadotwisty", "PuppayDog Eyes", 1000, move_list =
         {
             "Puppay": {"damage": 150, "accuracy": 50},
             "Tornadotwisty": {"damage": 350, "accuracy": 20},
             "PuppayDog Eyes": {"damage": 50, "accuracy": 90}
         })
-        self.kittaycat = Player("cat", "the-wip", "kungfu", 100, move_list =
+        self.kittaycat = Player("cat", "the-wip", "kungfu", 1000, move_list =
         {
             "cat": {"damage": 130, "accuracy": 65 },
             "The-wip": {"damage": 370, "accuracy": 7},
@@ -56,48 +56,57 @@ class Game:
                 choosing = False
             else: 
                 print("Invalid response. Please pick a or b.")
-            
 
-        self.player_turn = True # False will mean the computers turn
-        print(self.player_turn)
+        self.player_turn = random.choice([True, False]) # True will means it's your turn, False will mean the computers turn.
+        self.computer = self.kittaycat if self.player == self.puppaydog else self.puppaydog
 
-        if self.player == self.puppaydog:
-            self.computer = self.kittaycat
-        elif self.player == self.kittaycat:
-            self.computer = self.puppaydog
-        else: 
-            print("Error")
- 
     def turn(self):
         """This method should show the current health of both players,
         and allow the player to select a move to use on the opponent
         If it is the computer player's turn, it should select a move at random"""
+        print(f"{self.player.name}'s health: {self.player.hp} | {self.computer.name}'s health: {self.computer.hp}")
         if self.player_turn:
             if self.player == self.puppaydog: 
                 move = input("Pick your attack: Puppay (a), Tornadotwisty (b), or PuppayDog Eyes (c) >> ")
-                if move == "Puppay":
-                    #self.computer.hp -= self.move_list["Puppay"]["damage"]
-                    #print(self.computer.hp)
-            elif self.player == self.kittaycat:
-                move = input("Pick your attack: cat (a), The-wip (b), or kunfu (c) >>")
-            if move == "quit":
-                self.exit()
-            elif move == "r":
-                self.playing = False
-            #else:
-                #player.attack(computer)
- 
+                if move == "a":
+                    move_name = "Puppay"
+                elif move == "b":
+                    move_name = "Tornadotwisty"
+                elif move == "c":
+                    move_name = "PuppayDog Eyes"
+                elif move == "quit":
+                    self.exit()
+                else:
+                    print("Invalid move. Try again.")
+                    return
+                self.attack(self.player, self.computer, move_name)
+            else:
+                move_name = random.choice(list(self.computer.move_list.keys()))
+                self.attack(self.computer, self.player, move_name)
+
+            self.player_turn = not self.player_turn
+
+    def attack(self, attacker, defender, move_name):
+        move = attacker.move_list[move_name]
+        damage = move["damage"]
+        accuracy = move["accuracy"]
+
+        if random.randint(1, 100) <= accuracy:
+            defender.hp -= damage
+            print(f"{attacker.name} attacks {defender.name} with {move_name} for {damage} damage!")
         else:
-            #move = random.choice(self.computer.movelist) or movelist.kes() if dict
-            #computer.attack(player)
-            pass
- 
-        self.player_turn = not self.player_turn
+            print(f"{attacker.name}'s attack missed")
+
  
     def check_winner(self):
         """This method should check if either player's health has reached 0
         If a player's health has reached 0, it should display the winner"""
-        pass
+        if self.player.hp <= 0: 
+            print(f"{self.computer.name} ROCKED YO SHI")
+            self.playing = False
+        elif self.computer.hp <= 0: 
+            print(f"{self.player.name} got lucky")
+            self.playing = False
  
     def exit(self):
         """This method should allow the player to exit the game"""
@@ -105,31 +114,13 @@ class Game:
  
  
     def run(self):
-        self.player_turn = random.choice([True, False])
         self.playing = True
         while self.playing:
-            while True:
-                self.turn()
-                self.check_winner()
+            self.turn()
+            self.check_winner()
        
- 
- 
- 
-   
 def main():
-    playing = True
-    while playing:
-        game = Game()
-        game.run()
- 
- 
- 
-#This calls the main function
     game = Game()
-    game.turn(1)
-    game.check_winner()
-    game.turn(2)
-    game.check_winner()
+    game.run()
  
-#This calls the main function
 main()
